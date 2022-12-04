41b543f7c4991792e75dabce09fb88e613d0b5c9
MahmoudEhabMorsy <mahmoudehab30@gmail.com>
2022-12-04 20:05:37 +0200

    test cases fixed

--- Test Cases/DIO TEST/Code/dio_writePort_test.c
+++ Test Cases/DIO TEST/Code/dio_writePort_test.c
@@ -6,15 +6,15 @@ void DIO_writePort_test(void) {
 	DIO_setupPortDirection(PORTC_ID, PORT_OUTPUT);
 	DIO_setupPortDirection(PORTD_ID, PORT_OUTPUT);
 	while (1) {
-		DIO_writePort(PORTA_ID, LOGIC_HIGH);
-		DIO_writePort(PORTA_ID, LOGIC_HIGH);
-		DIO_writePort(PORTA_ID, LOGIC_HIGH);
-		DIO_writePort(PORTA_ID, LOGIC_HIGH);
+		DIO_writePort(PORTA_ID, 0xff);
+		DIO_writePort(PORTB_ID, 0xff);
+		DIO_writePort(PORTC_ID, 0xff);
+		DIO_writePort(PORTD_ID, 0xff);
 		_delay_ms(1000);
-		DIO_writePort(PORTA_ID, LOGIC_LOW);
-		DIO_writePort(PORTA_ID, LOGIC_LOW);
-		DIO_writePort(PORTA_ID, LOGIC_LOW);
-		DIO_writePort(PORTA_ID, LOGIC_LOW);
+		DIO_writePort(PORTA_ID, 0);
+		DIO_writePort(PORTB_ID, 0);
+		DIO_writePort(PORTC_ID, 0);
+		DIO_writePort(PORTD_ID, 0);
 		_delay_ms(1000);
 
 	}
