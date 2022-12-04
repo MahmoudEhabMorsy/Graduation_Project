778dac4cb7e4b20cc76d503bbeb123bffb7571a3
MahmoudEhabMorsy <mahmoudehab30@gmail.com>
2022-12-04 17:55:39 +0200

    small fix

--- io.h
+++ io.h
@@ -4,21 +4,21 @@
  /*-----------------------------------------------------------------------*
  |                                  DIO                                   |
  *-----------------------------------------------------------------------*/
-#define PORTA (*volatile uint8)(0x3B)
-#define DDRA (*volatile uint8)(0x3A)
-#define PINA (*volatile uint8)(0x39)
+#define PORTA *((*volatile uint8)0x3B)
+#define DDRA *((*volatile uint8)0x3A)
+#define PINA *((*volatile uint8)0x39)
 
-#define PORTB (*volatile uint8)(0x38)
-#define DDRB (*volatile uint8)(0x37)
-#define PINB (*volatile uint8)(0x36)
+#define PORTB *((*volatile uint8)0x38)
+#define DDRB *((*volatile uint8)0x37)
+#define PINB *((*volatile uint8)0x36)
 
-#define PORTC (*volatile uint8)(0x35)
-#define DDRC (*volatile uint8)(0x34)
-#define PINC (*volatile uint8)(0x33)
+#define PORTC *((*volatile uint8)0x35)
+#define DDRC *((*volatile uint8)0x34)
+#define PINC *((*volatile uint8)0x33)
 
-#define PORTD (*volatile uint8)(0x32)
-#define DDRD (*volatile uint8)(0x31)
-#define PIND (*volatile uint8)(0x30)
+#define PORTD *((*volatile uint8)0x32)
+#define DDRD *((*volatile uint8)0x31)
+#define PIND *((*volatile uint8)0x30)
 
  /*-----------------------------------------------------------------------*
  |                                   ADC                                  |
