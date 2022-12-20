822e60fde2f489509b95ac3f969782c8dbacc3f2
MahmoudEhabMorsy <mahmoudehab30@gmail.com>
2022-12-20 20:12:59 +0200

    added another test case

--- Test Cases/DIO TEST/Code/dio_readPin_test.c
+++ Test Cases/DIO TEST/Code/dio_readPin_test.c
@@ -2,80 +2,44 @@
 
 void DIO_readPin_test(void) {
 	DIO_setupPinDirection(PORTA_ID, PIN0_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTA_ID, PIN1_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTA_ID, PIN2_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTA_ID, PIN3_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTA_ID, PIN4_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTA_ID, PIN5_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTA_ID, PIN6_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTA_ID, PIN7_ID, PIN_INPUT);
+	DIO_setupPinDirection(PORTA_ID, PIN1_ID, PIN_OUTPUT);
 
 	DIO_setupPinDirection(PORTB_ID, PIN0_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTB_ID, PIN1_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTB_ID, PIN2_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTB_ID, PIN3_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTB_ID, PIN4_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTB_ID, PIN5_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTB_ID, PIN6_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTB_ID, PIN7_ID, PIN_INPUT);
+	DIO_setupPinDirection(PORTB_ID, PIN1_ID, PIN_OUTPUT);
 
 	DIO_setupPinDirection(PORTC_ID, PIN0_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTC_ID, PIN1_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTC_ID, PIN2_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTC_ID, PIN3_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTC_ID, PIN4_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTC_ID, PIN5_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTC_ID, PIN6_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTC_ID, PIN7_ID, PIN_INPUT);
+	DIO_setupPinDirection(PORTC_ID, PIN1_ID, PIN_OUTPUT);
 
 	DIO_setupPinDirection(PORTD_ID, PIN0_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTD_ID, PIN1_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTD_ID, PIN2_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTD_ID, PIN3_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTD_ID, PIN4_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTD_ID, PIN5_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTD_ID, PIN6_ID, PIN_INPUT);
-	DIO_setupPinDirection(PORTD_ID, PIN7_ID, PIN_INPUT);
+	DIO_setupPinDirection(PORTD_ID, PIN1_ID, PIN_OUTPUT);
 
 	/*activating internal pull up circuit*/
+
 	DIO_writePin(PORTA_ID, PIN0_ID, LOGIC_HIGH);
-	DIO_writePin(PORTA_ID, PIN1_ID, LOGIC_HIGH);
-	DIO_writePin(PORTA_ID, PIN2_ID, LOGIC_HIGH);
-	DIO_writePin(PORTA_ID, PIN3_ID, LOGIC_HIGH);
-	DIO_writePin(PORTA_ID, PIN4_ID, LOGIC_HIGH);
-	DIO_writePin(PORTA_ID, PIN5_ID, LOGIC_HIGH);
-	DIO_writePin(PORTA_ID, PIN6_ID, LOGIC_HIGH);
-	DIO_writePin(PORTA_ID, PIN7_ID, LOGIC_HIGH);
 
 	DIO_writePin(PORTB_ID, PIN0_ID, LOGIC_HIGH);
-	DIO_writePin(PORTB_ID, PIN1_ID, LOGIC_HIGH);
-	DIO_writePin(PORTB_ID, PIN2_ID, LOGIC_HIGH);
-	DIO_writePin(PORTB_ID, PIN3_ID, LOGIC_HIGH);
-	DIO_writePin(PORTB_ID, PIN4_ID, LOGIC_HIGH);
-	DIO_writePin(PORTB_ID, PIN5_ID, LOGIC_HIGH);
-	DIO_writePin(PORTB_ID, PIN6_ID, LOGIC_HIGH);
-	DIO_writePin(PORTB_ID, PIN7_ID, LOGIC_HIGH);
 
 	DIO_writePin(PORTC_ID, PIN0_ID, LOGIC_HIGH);
-	DIO_writePin(PORTC_ID, PIN1_ID, LOGIC_HIGH);
-	DIO_writePin(PORTC_ID, PIN2_ID, LOGIC_HIGH);
-	DIO_writePin(PORTC_ID, PIN3_ID, LOGIC_HIGH);
-	DIO_writePin(PORTC_ID, PIN4_ID, LOGIC_HIGH);
-	DIO_writePin(PORTC_ID, PIN5_ID, LOGIC_HIGH);
-	DIO_writePin(PORTC_ID, PIN6_ID, LOGIC_HIGH);
-	DIO_writePin(PORTC_ID, PIN7_ID, LOGIC_HIGH);
 
 	DIO_writePin(PORTD_ID, PIN0_ID, LOGIC_HIGH);
-	DIO_writePin(PORTD_ID, PIN1_ID, LOGIC_HIGH);
-	DIO_writePin(PORTD_ID, PIN2_ID, LOGIC_HIGH);
-	DIO_writePin(PORTD_ID, PIN3_ID, LOGIC_HIGH);
-	DIO_writePin(PORTD_ID, PIN4_ID, LOGIC_HIGH);
-	DIO_writePin(PORTD_ID, PIN5_ID, LOGIC_HIGH);
-	DIO_writePin(PORTD_ID, PIN6_ID, LOGIC_HIGH);
-	DIO_writePin(PORTD_ID, PIN7_ID, LOGIC_HIGH);
 
 	while (1) {
-		/*needs LCD to display outputs(inputs to CPU)*/
+		if(!DIO_readPin(PORTA_ID,PIN0_ID)){
+			DIO_writePin(PORTA_ID,PIN1_ID,LOGIC_HIGH);
+
+		}
+		if(!DIO_readPin(PORTB_ID,PIN0_ID)){
+			DIO_writePin(PORTB_ID,PIN1_ID,LOGIC_HIGH);
+
+		}
+		if(!DIO_readPin(PORTC_ID,PIN0_ID)){
+			DIO_writePin(PORTC_ID,PIN1_ID,LOGIC_HIGH);
+
+		}
+		if(!DIO_readPin(PORTD_ID,PIN0_ID)){
+			DIO_writePin(PORTD_ID,PIN1_ID,LOGIC_HIGH);
+
+		}
 	}
 
 }
--- Test Cases/DIO TEST/Code/dio_readPort_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIO_READPORT_TEST_H_
-#define DIO_READPORT_TEST_H_
-
-#include"../../../dio.h"
-#include<util/delay.h>
-void DIO_readPort_test(void);
-
-
-#endif
