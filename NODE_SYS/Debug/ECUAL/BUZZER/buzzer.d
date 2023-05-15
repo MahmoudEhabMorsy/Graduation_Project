19f56deec637cbfe71bda2920d6a860693a939d4
Ahmed Yasser Kaead <67479469+AhmedYKaead@users.noreply.github.com>
2023-05-16 02:31:24 +0300

    Add files via upload
    A nodal system that is responsible for measuring pressure and temperature using bmp180,  distance using ultrasonic and pumping upon need.

---
+++ NODE_SYS/Debug/ECUAL/BUZZER/buzzer.d
@@ -0,0 +1,12 @@
+ECUAL/BUZZER/buzzer.o ECUAL/BUZZER/buzzer.o: ../ECUAL/BUZZER/buzzer.c \
+  ../ECUAL/BUZZER/../../MCAL/DIO/dio.h \
+  ../ECUAL/BUZZER/../../MCAL/DIO/../../CommonDef/std_types.h \
+  ../ECUAL/BUZZER/../../MCAL/DIO/io.h ../ECUAL/BUZZER/buzzer.h
+
+../ECUAL/BUZZER/../../MCAL/DIO/dio.h:
+
+../ECUAL/BUZZER/../../MCAL/DIO/../../CommonDef/std_types.h:
+
+../ECUAL/BUZZER/../../MCAL/DIO/io.h:
+
+../ECUAL/BUZZER/buzzer.h:
---
+++ NODE_SYS/Debug/MCAL/timer.d
@@ -0,0 +1,11 @@
+MCAL/timer.o MCAL/timer.o: ../MCAL/timer.c ../MCAL/timer.h \
+  ../MCAL/../CommonDef/std_types.h ../MCAL/../CommonDef/common_macros.h \
+  ../MCAL/register_TIMER.h
+
+../MCAL/timer.h:
+
+../MCAL/../CommonDef/std_types.h:
+
+../MCAL/../CommonDef/common_macros.h:
+
+../MCAL/register_TIMER.h:
