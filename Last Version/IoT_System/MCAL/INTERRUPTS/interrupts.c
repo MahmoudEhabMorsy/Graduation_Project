a42037e676da34eb4051c81b09cf7cdcd1175b6b
MQotp <mahmoud.abolfadl@gmail.com>
2023-06-20 18:52:33 +0300

    Kattar 5eer el denia

---
+++ Last Version/IoT_System/MCAL/INTERRUPTS/interrupts.c
@@ -0,0 +1,28 @@
+/*
+ * interrupts.c
+ *
+ *  Created on: Jun 20, 2023
+ *      Author: Mahmoud Qotp
+ */
+
+#include "interrupts.h"
+
+uint8 g_INT1_Flag = 0;
+
+ISR(INT1_vect)
+{
+	// set the interrupt flag to indicate that INT1 occurs
+	g_INT1_Flag = 1;
+
+	//Start the SPI to communication with the sensing control node
+}
+
+/* External INT1 enable and configuration function */
+void INT1_Init(void)
+{
+	DDRD  &= (~(1<<PD3));  // Configure INT1/PD3 as input pin
+	PORTD &=~(1<<PD3); //Initially LOW
+	/*Trigger INT1 with the rising edge*/
+	MCUCR |= (1<<ISC11) | (1 << ISC10);
+	GICR  |= (1<<INT1);    // Enable external interrupt pin INT1
+}
---
+++ Last Version/IoT_System/MCAL/INTERRUPTS/interrupts.h
@@ -0,0 +1,18 @@
+/*
+ * interrupts.h
+ *
+ *  Created on: Jun 20, 2023
+ *      Author: Mahmoud Qotp
+ */
+
+#ifndef MCAL_INTERRUPTS_INTERRUPTS_H_
+#define MCAL_INTERRUPTS_INTERRUPTS_H_
+
+#include <avr/interrupt.h>
+#include "../../UTILITIES/std_types.h"
+#include <avr/io.h>
+
+void INT1_Init(void);
+
+
+#endif /* MCAL_INTERRUPTS_INTERRUPTS_H_ */
