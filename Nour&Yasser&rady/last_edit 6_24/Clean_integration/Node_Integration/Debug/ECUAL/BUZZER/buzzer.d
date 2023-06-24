d6ff82efc18b5e436d79f77a383374ba1641ac88
RadyMagdy <85202622+RadyMagdy@users.noreply.github.com>
2023-06-24 19:57:30 +0300

    Add files via upload
    last updated integration for sensing node

---
+++ Nour&Yasser&rady/last_edit 6_24/Clean_integration/Node_Integration/Debug/ECUAL/BUZZER/buzzer.d
@@ -0,0 +1,9 @@
+ECUAL/BUZZER/buzzer.o ECUAL/BUZZER/buzzer.o: ../ECUAL/BUZZER/buzzer.c \
+  ../ECUAL/BUZZER/buzzer.h ../ECUAL/BUZZER/../../MCAL/DIO/dio.h \
+  ../ECUAL/BUZZER/../../MCAL/DIO/../../CommonDef/std_types.h
+
+../ECUAL/BUZZER/buzzer.h:
+
+../ECUAL/BUZZER/../../MCAL/DIO/dio.h:
+
+../ECUAL/BUZZER/../../MCAL/DIO/../../CommonDef/std_types.h:
---
+++ Nour&Yasser&rady/last_edit 6_24/Clean_integration/Node_Integration/Debug/MCAL/ICU/icu.d
@@ -0,0 +1,9 @@
+MCAL/ICU/icu.o MCAL/ICU/icu.o: ../MCAL/ICU/icu.c ../MCAL/ICU/icu.h \
+  ../MCAL/ICU/../../CommonDef/std_types.h \
+  ../MCAL/ICU/../../CommonDef/common_macros.h
+
+../MCAL/ICU/icu.h:
+
+../MCAL/ICU/../../CommonDef/std_types.h:
+
+../MCAL/ICU/../../CommonDef/common_macros.h:
