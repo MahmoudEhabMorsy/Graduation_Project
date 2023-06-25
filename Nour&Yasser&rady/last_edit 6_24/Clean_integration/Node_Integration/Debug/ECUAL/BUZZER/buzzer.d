a5381e5ca75e9a8537b34372814806f70800a2db
RadyMagdy <85202622+RadyMagdy@users.noreply.github.com>
2023-06-25 16:57:27 +0300

    Delete Nour&Yasser&rady/last_edit 6_24/Clean_integration directory

--- Nour&Yasser&rady/last_edit 6_24/Clean_integration/Node_Integration/Debug/ECUAL/BUZZER/buzzer.d
+++
@@ -1,9 +0,0 @@
-ECUAL/BUZZER/buzzer.o ECUAL/BUZZER/buzzer.o: ../ECUAL/BUZZER/buzzer.c \
-  ../ECUAL/BUZZER/buzzer.h ../ECUAL/BUZZER/../../MCAL/DIO/dio.h \
-  ../ECUAL/BUZZER/../../MCAL/DIO/../../CommonDef/std_types.h
-
-../ECUAL/BUZZER/buzzer.h:
-
-../ECUAL/BUZZER/../../MCAL/DIO/dio.h:
-
-../ECUAL/BUZZER/../../MCAL/DIO/../../CommonDef/std_types.h:
--- Nour&Yasser&rady/last_edit 6_24/Clean_integration/Node_Integration/Debug/MCAL/TIMER/timer.d
+++
@@ -1,9 +0,0 @@
-MCAL/TIMER/timer.o MCAL/TIMER/timer.o: ../MCAL/TIMER/timer.c \
-  ../MCAL/TIMER/timer.h ../MCAL/TIMER/../../CommonDef/std_types.h \
-  ../MCAL/TIMER/../../CommonDef/common_macros.h
-
-../MCAL/TIMER/timer.h:
-
-../MCAL/TIMER/../../CommonDef/std_types.h:
-
-../MCAL/TIMER/../../CommonDef/common_macros.h:
